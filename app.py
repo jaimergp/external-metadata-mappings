8fd8a4dc7d3c8f88d09d15270442898ea4fdaf20
jaimergp <jaimergp@users.noreply.github.com>
2025-02-20 11:04:06 +0100

    Add URL navigation

--- app.py
+++ app.py
@@ -57,26 +57,62 @@ def mapping(ecosystem):
 
 def mappings_for_purl(purl, ecosystem):
     for m in mapping(ecosystem).get("mappings", ()):
-        if m["id"] == purl:
+        if m["id"] == purl and m.get("specs") or m.get("specs_from"):
             yield m
 
+def parse_url():
+    params = st.query_params.to_dict()
+    purl = params.pop("purl", None)
+    ecosystem = params.pop("ecosystem", None)
+    purltype = params.pop("purltype", None)
+    if params:
+        st.warning(f"URL contains unknown elements: {st.query_params.to_dict}")
+        st.stop()
+    return {"purl": purl, "ecosystem": ecosystem, "purltype": purltype}
+    
+
+url_params = parse_url()
+if not getattr(st.session_state, "initialized", False):
+    initialized = False
+    for param, value in url_params.items():
+        if value:
+            setattr(st.session_state, param, value)
+            initialized = True
+    if initialized:
+        st.session_state.initialized = initialized
+    
 
 with st.sidebar:
     purltype = st.segmented_control(
         "Filter by PURL type",
         options=["Virtual", "Generic", "Other"],
+        key="purltype",
     )
-    purl = st.selectbox("PURL", options=["", *sorted(all_purls(purltype))])
-    ecosystem = st.selectbox("Ecosystem", options=["", *ecosystems()])
+    purl = st.selectbox(
+        "PURL",
+        options=sorted(all_purls(purltype)),
+        index=None,
+        key="purl",
+        placeholder="Choose a PURL identifier"
+    )
+    ecosystem = st.selectbox(
+        "Ecosystem",
+        options=[eco for eco in ecosystems() if list(mappings_for_purl(purl, eco))] if purl else [],
+        index=None,
+        key="ecosystem",
+        placeholder="Choose a target ecosystem",
+        help="Target ecosystem for which to show the mapping. Only enabled if mappings are found."
+    )
 
 # Mappings detail page
 if purl and ecosystem:
+    st.query_params.clear()
+    st.query_params.purl = purl
+    st.query_params.ecosystem = ecosystem
+    if purltype:
+        st.query_params.purltype = purltype
     this_mapping = mapping(ecosystem)
-    found_mappings = [
-        m
-        for m in mappings_for_purl(purl, ecosystem)
-        if m.get("specs") or m.get("specs_from")
-    ]
+    found_mappings = list(mappings_for_purl(purl, ecosystem))
     st.write(f"# `{purl}`")
     st.write(f"{len(found_mappings)} mapping(s) found for {ecosystem.title()}")
     for m in found_mappings:
@@ -116,6 +152,10 @@ if purl and ecosystem:
             st.write("Not available in this ecosystem.")
 # Identifier detail page
 elif purl:
+    st.query_params.clear()
+    st.query_params.purl = purl
+    if purltype:
+        st.query_params.purltype = purltype
     provided_by = []
     for d in registry()["definitions"]:
         if d["id"] == purl:
@@ -133,6 +173,9 @@ elif purl:
             st.write(f"- `{prov}`")
 # All identifiers list
 else:
+    st.query_params.clear()
+    if purltype:
+        st.query_params.purltype = purltype
     definitions = registry()["definitions"]
     st.write(f"We found {len(definitions)} definitions:")
     for i, d in enumerate(definitions, 1):
