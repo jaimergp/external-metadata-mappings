dd7576e93acdba65121b155412f2b15eccd5c202
jaimergp <jaimergp@users.noreply.github.com>
2025-03-11 12:01:53 +0100

    Play a bit with `univers` and version parsing

--- src/external_metadata_mappings/parse.py
+++ src/external_metadata_mappings/parse.py
@@ -3,8 +3,16 @@ Parse dep: dependencies
 """
 
 from packageurl import PackageURL
-
+from univers.versions import PypiVersion
+from univers.version_range import PypiVersionRange
 
 class DepURL(PackageURL):
     # TODO: Needs https://github.com/package-url/packageurl-python/pull/184
     SCHEME = "dep"
+
+    def parse_version(self):
+        if not self.version:
+            return None
+        if self.version[0].isdigit():
+            return PypiVersion.build_value(self.version)
+        return PypiVersionRange.from_native(self.version)
