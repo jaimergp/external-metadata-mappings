7937664776cbb8ee8c0b21129a145325b41576ce
jaimergp <jaimergp@users.noreply.github.com>
2025-06-20 12:25:34 +0200

    Ensure `virtual:` deps don't have `provides` (#11)

---
+++ scripts/lint-registry.py
@@ -0,0 +1,17 @@
+import sys
+import json
+from pathlib import Path
+
+exit_code = 0
+
+for path in sys.argv[1:]:
+    path = Path(path)
+    registry = json.loads(path.read_text())
+    for definition in registry["definitions"]:
+        if definition["id"].startswith("dep:virtual/") and definition.get("provides"):
+            print(
+                f"'{definition['id']}' is a virtual DepURL and cannot have 'provides'."
+            )
+            exit_code = 1
+
+sys.exit(exit_code)
